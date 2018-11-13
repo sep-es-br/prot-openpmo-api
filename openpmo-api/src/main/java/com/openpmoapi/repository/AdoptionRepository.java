f22c6117b9976d88c7644ad3997c743701c45c34
lucas.regio <lucas.regio@PEPGG10.planejamento.es.gov.br>
2018-11-13 17:10:45 -0200

    Swagger

--- openpmo-api/src/main/java/com/openpmoapi/repository/AdoptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openpmoapi.repository;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-import com.openpmoapi.model.Adoption;
-
-public interface AdoptionRepository extends Neo4jRepository <Adoption, Long>{
-
-}
