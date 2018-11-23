d38e10fa8f5fdc43eb00c05fa7ba1c4cb8e46f29
marcos.santos <marcos.santos@PTI02.planejamento.es.gov.br>
2018-11-23 17:10:06 -0200

    update

--- openpmo-master/openpmo-api/src/main/java/com/openpmoapi/model/Person.java
+++ openpmo-master/openpmo-api/src/main/java/com/openpmoapi/model/Person.java
@@ -59,8 +59,20 @@ public class Person {
 
 
 	public String password;
-	
-	
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 
 	private String fullName;
 	/**
