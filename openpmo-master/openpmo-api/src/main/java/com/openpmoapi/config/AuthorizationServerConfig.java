2c5dce8f9ef17e8560fd6c5b3f77f8f20749aae1
Lucas Regio <lucasregio@users.noreply.github.com>
2018-11-28 17:38:20 -0200

    Update Oauth

---
+++ openpmo-master/openpmo-api/src/main/java/com/openpmoapi/config/AuthorizationServerConfig.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.openpmoapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+/**
+* Type here a brief description of the class.
+*
+* @author lucas.regio Lucas Regio 
+* @since 2018-11-28
+*/
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("angular")
+			.secret("@ngul@r0")
+			.scopes("read", "write")
+			.authorizedGrantTypes("password")
+			.accessTokenValiditySeconds(1800);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.tokenStore(tokenStore())
+			.authenticationManager(authenticationManager);
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+	
+}
---
+++ openpmo-master/openpmo-api/src/main/java/com/openpmoapi/config/ResourceServerConfig.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.openpmoapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+/**
+* Type here a brief description of the class.
+*
+* @author lucas.regio Lucas Regio 
+* @since 2018-11-28
+*/
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+	@Configuration
+	@EnableWebSecurity
+	@EnableResourceServer
+	public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+		@Autowired
+		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication().withUser("teste").password(passwordEncoder().encode("123")).roles("ADMIN").and()
+					.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+		}
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+			.antMatchers("/categorias").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.csrf().disable();
+		}
+		
+		@Override
+		public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+			resources.stateless(true);
+		}
+
+		@Bean
+		static PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+	
+}
