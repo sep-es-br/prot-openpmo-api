ef5c2d59b378323f7e33a40997020403dd0de374
Lucas Regio <lucasregio@users.noreply.github.com>
2018-12-06 17:07:14 -0200

    javadoc update

--- openpmo-master/openpmo-api/src/main/java/com/openpmoapi/util/Util.java
+++ openpmo-master/openpmo-api/src/main/java/com/openpmoapi/util/Util.java
@@ -6,7 +6,7 @@ package com.openpmoapi.util;
 import java.text.Normalizer;
 
 /**
-* Type here a brief description of the class.
+* This is a class of utility methods, like remove special characters.
 *
 * @author marcos.santos  
 * @since 2018-08-24
